666a2dd103464c53e965a816b83b36e6feb93ab1
lovew <lovewlever@outlook.com>
2020-06-20 21:44:50 +0800

    增加倒计时

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,13 +1,34 @@
 <template>
     <div class="f-six">
-        <img style="margin: auto" src="../assets/welcome_loading.gif" alt="loading...">
+        <!--<img style="margin: auto" src="../assets/welcome_loading.gif" alt="loading...">-->
+        <div class="progress" style="margin: auto;width: 50%">
+            <div class="progress-bar" role="progressbar" :style="{width: progress + '%'}" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                {{progress}}%</div>
+        </div>
     </div>
 
 </template>
 
 <script>
     export default {
-        name: "Welcome"
+        name: "Welcome",
+        data () {
+            return {
+                progress: 0,
+                timer:null
+            }
+        },
+        created() {
+            this. timer = setInterval(this.countDownTimer,100)
+        },
+        methods: {
+            countDownTimer: function() {
+                this.progress += 5
+                if (this.progress >= 100) {
+                    clearInterval(this.timer)
+                }
+            }
+        }
     }
 </script>
 
